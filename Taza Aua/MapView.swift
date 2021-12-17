5e7e4b724dc6cd52ddc2143b7a6a667c1df88726
Btkm2 <beketmuratbek57@gmail.com>
2021-12-17 12:02:51 +0600

    Implementing changes with different appearances, creating some sort of complex UI

--- Taza Aua/MapView.swift
+++ Taza Aua/MapView.swift
@@ -20,13 +20,27 @@ struct MapView: View {
             center: CLLocationCoordinate2D(latitude: 43.249_958, longitude: 76.921_152),
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         )
+    @State private var searchQuery: String = ""
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+        ZStack{
+            Map(coordinateRegion: $region, showsUserLocation: true, userTrackingMode: .constant(.follow))
+            VStack{
+                TextField("Search for something...", text: $searchQuery)
+                    .padding()
+                    .background(Color("DarkBlue"))
+                    .foregroundColor(Color("IconColor"))
+                    .frame(width: UIScreen.main.bounds.width-50, height: 50)
+                    .cornerRadius(20)
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
+            .preferredColorScheme(.light)
     }
 }
--- Taza Aua/ShopView.swift
+++ Taza Aua/ShopView.swift
@@ -32,7 +32,7 @@ struct ShopView: View {
                 Spacer()
             }
 //            Spacer()
-            VStack{
+            VStack(alignment: .leading){
                 RoundedRectangle(cornerRadius: 0)
                     .fill(Color("DarkBlue"))
                     .frame(width: UIScreen.main.bounds.width, height: 1)
@@ -41,20 +41,96 @@ struct ShopView: View {
                         HStack{
                             Image(systemName: "magnifyingglass")
                             TextField("Search",text: $searchItem)
+//                                .background(Color("DarkBlue"))
                             //.textFieldStyle(RoundedBorderTextFieldStyle())
         //                        .frame(width: UIScreen.main.bounds.width-20, height: 50)
                             Image(systemName: "mic")
                         }
                         .padding(.all, 8)
-                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(lineWidth: 1).foregroundColor(Color.gray))
+                        .overlay(RoundedRectangle(cornerRadius: 15).stroke(lineWidth: 1).foregroundColor(Color("DarkBlue")).background(Color("arkBlue")))
                         .foregroundColor(Color("IconColor"))
-                        .frame(width: UIScreen.main.bounds.width-30, height: 50)
-                    Image(systemName: "slider.horizontal.3")
-                        .foregroundColor(Color("IconColor"))
+                        .frame(width: UIScreen.main.bounds.width-50, height: 50)
+//                    Image(systemName: "slider.horizontal.3")
+////                        .renderingMode(.original)
+////                        .resizable()
+////                        .aspectRatio(contentMode: .fit)
+//                        .foregroundColor(Color.blue)
+//                        //.padding(.all,0)
+//                        .frame(width: 40, height: 40)
+//                        .background(Color("DarkBlue"))
+//                        .cornerRadius(10)
+                    Button(action: {}){
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                    .foregroundColor(Color.blue)
+                    //.padding(.all,0)
+                    .frame(width: 40, height: 40)
+                    .background(Color("DarkBlue"))
+                    .cornerRadius(10)
+                }
+                ScrollView(.horizontal){
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("DarkBlue"))
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.2)
+                        .padding(.top, 20)
                 }
 //                .frame(width: UIScreen.main.bounds.width-30, height: 50)
                 Spacer()
             }
+            VStack(alignment: .leading){
+                HStack{
+                    Text("Католог товаров")
+                        .padding(.trailing, 160)
+                    Button(action: {}){
+                        HStack{
+                            Text("Все")
+                            Image(systemName: "arrow.right")
+                        }
+                        .frame(width: 80, height: 35)
+                        .background(Color("DarkBlue"))
+                        .foregroundColor(Color.blue)
+                        .cornerRadius(12)
+                    }
+                    .padding(.trailing, -10)
+                }
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack{
+                        ForEach(0..<8){ index in
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color("DarkBlue"))
+                                .frame(width: 100, height: 40)
+                        }
+                    }
+                }
+            }
+            .padding(.top, 30)
+            VStack(alignment: .leading){
+                HStack{
+                    Text("Защитные маски для лица")
+                        .padding(.trailing, 160)
+                    Button(action: {}){
+                        HStack{
+                            Text("Все")
+                            Image(systemName: "arrow.right")
+                        }
+                        .frame(width: 80, height: 35)
+                        .background(Color("DarkBlue"))
+                        .foregroundColor(Color.blue)
+                        .cornerRadius(12)
+                    }
+                    .padding(.trailing, -10)
+                }
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack{
+                        ForEach(0..<6){ index in
+                            RoundedRectangle(cornerRadius: 15)
+                                .fill(Color("DarkBlue"))
+                                .frame(width: 150, height: 150)
+                        }
+                    }
+                }
+            }
+            .padding(.top, 350)
            // Spacer()
 //            ScrollView(.horizontal){
 //                HStack{
