6ec7b6a4d51834e7a766ab63ac56bc7a28f75d84
Btkm2 <beketmuratbek57@gmail.com>
2021-12-17 07:04:59 +0600

    Initial Commit

---
+++ Taza Aua/Taza_AuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taza_AuaApp.swift
+//  Taza Aua
+//
+//  Created by Beket Muratbek on 17.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Taza_AuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
